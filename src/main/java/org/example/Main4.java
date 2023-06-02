4a07a205e80f9750cbab99f10fb2f1e174cf8af5
cecilioAlvarez <cmac@arquitecturajava.com>
2023-06-02 11:55:36 +0200

    filter by concept

---
+++ src/main/java/org/example/Main4.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main4 {
+    public static void main(String[] args) {
+
+        List<Factura> lista = new ArrayList<Factura>();
+        lista.add(new Factura(3, "ordenador", 200));
+        lista.add(new Factura(2, "tablet", 300));
+        lista.add(new Factura(1, "auricular", 50));
+
+        List<Factura> filtrada= buscarPorConcepto("ordenador", lista);
+        for (Factura f: filtrada) {
+            System.out.println(f.getNumero());
+            System.out.println(f.getConcepto());
+            System.out.println(f.getImporte());
+
+        }
+
+    }
+    public static List<Factura> buscarPorConcepto(String concepto, List<Factura> lista) {
+        List<Factura> listaFinal = new ArrayList<Factura>();
+
+
+        for (Factura f: lista) {
+
+            if (f.getConcepto().equals(concepto)) {
+
+                listaFinal.add(f);
+            }
+         }
+        return listaFinal;
+    }
+}
