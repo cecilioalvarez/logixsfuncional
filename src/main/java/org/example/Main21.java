f5f16df081139c988d317186bf3c4e631fa15c2d
cecilioAlvarez <cmac@arquitecturajava.com>
2023-06-06 10:04:20 +0200

    maximos

--- src/main/java/org/example/Main21.java
+++ src/main/java/org/example/Main21.java
@@ -1,19 +1,16 @@
 package org.example;
 
 import java.util.List;
+import java.util.Optional;
 
 public class Main21 {
     public static void main(String[] args) {
 
+        Optional<Integer> maximo= List.of(5, 7, 2, 3, 4, 5, 6)
+               .stream().max(Integer::compare);
 
-
-
-
-       String resultado=List.of("5", "7", "2", "3", "4", "5", "6")
-               .stream()
-               .reduce("0", (acumulador, item) -> acumulador + "," +item);
-
-        System.out.println(resultado);
+        if(maximo.isPresent()) System.out.println(maximo.get());
+        System.out.println(maximo);
 
 
     }
